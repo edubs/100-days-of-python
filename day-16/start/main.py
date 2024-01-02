22c2dda13706f750d057915be4acda08f47e134d
edubs <11930020+edubs@users.noreply.github.com>
2024-01-02 13:33:32 -0500

    day 16 changes done

--- day-16/start/main.py
+++ day-16/start/main.py
@@ -1,8 +1,28 @@
-from turtle import Turtle, Screen
+from coffee_maker import CoffeeMaker
+from menu import Menu
+from money_machine import MoneyMachine
 
-timmy = Turtle()
-my_screen = Screen()
-timmy.shape("turtle")
-timmy.color("aquamarine")
-timmy.forward(100)
-my_screen.exitonclick()
+my_menu = Menu()
+my_coffee_maker = CoffeeMaker()
+my_money_machine = MoneyMachine()
+
+
+def print_report():
+    print(my_coffee_maker.report())
+    print(my_money_machine.report())
+
+
+while True:
+    customer_choice = input(f"What would you like? ({my_menu.get_items()}):")
+    if customer_choice == "off":
+        exit(0)
+    elif customer_choice == "report":
+        print_report()
+    else:
+        beverage_object = my_menu.find_drink(customer_choice)
+
+        if my_coffee_maker.is_resource_sufficient(beverage_object) and my_money_machine.make_payment(
+                beverage_object.cost):
+            print(f"enough resources to make the drink are available")
+            my_coffee_maker.make_coffee(beverage_object)
+            print_report()
