3c96f12f9b9cbd122e8ee16113d9a6016c585944
edubs <11930020+edubs@users.noreply.github.com>
2024-01-10 15:58:19 -0500

    day 20 updates

---
+++ Day-20/snake_game/snake.py
@@ -0,0 +1,47 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_turtle = Turtle(shape="square")
+            new_turtle.color("purple")
+            new_turtle.penup()
+            new_turtle.goto(position)
+            self.segments.append(new_turtle)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def move(self):
+        for seg in range(len(self.segments) - 1, 0, -1):
+            x = self.segments[seg - 1].xcor()
+            y = self.segments[seg - 1].ycor()
+            self.segments[seg].goto(x, y)
+        self.head.forward(MOVE_DISTANCE)
