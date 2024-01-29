6aaefc85ad74bdc62afae50fa4703feba866d737
Erik Williams <Erik.Williams@LibertyMutual.com>
2024-01-29 10:41:02 -0500

    day 24 mail merge update

--- day-24/mail-merge/create_letters.py
+++ day-24/mail-merge/create_letters.py
@@ -2,10 +2,12 @@ with open("./input/names/invite_names.txt") as names_file:
     names_list = names_file.read().splitlines()
 
 for name in names_list:
+    stripped_name = name.strip()
     with open("./input/letters/starting_letter.txt") as letter_base:
         letter_contents = letter_base.read()
-        letter_contents = letter_contents.replace("[name]", name)
+        letter_contents = letter_contents.replace("[name]", stripped_name)
+        stripped_name = name.replace(" ", "_")
 
-    letter_name = f"./output/readytosend/letter_for_{name}.txt"
+    letter_name = f"./output/readytosend/letter_for_{stripped_name}.txt"
     with open(letter_name, mode="a") as new_letter:
         new_letter.write(letter_contents)
