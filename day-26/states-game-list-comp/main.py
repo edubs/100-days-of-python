d31629d619d383b0a5ca6415be764be2d89a3caa
Erik Williams <Erik.Williams@LibertyMutual.com>
2024-02-21 12:02:06 -0500

    26 - states game solution from class

--- day-26/states-game-list-comp/main.py
+++ day-26/states-game-list-comp/main.py
@@ -1,41 +1,32 @@
 import turtle
-
 import pandas
 
 screen = turtle.Screen()
-screen.title("US States Game")
-
+screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-data = pandas.read_csv('50_states.csv')
-list_states = data.state.to_list()
-game_is_on = True
-corrects = 0
-list_correct = []
+data = pandas.read_csv("../../../../Downloads/day-25-solution-us-states-game-end/50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
 
-while game_is_on:
-    answer_state = screen.textinput(title=f"{corrects}/50 States Correct",
-                                    prompt="What's another state's name?").title()  # saves doing it on another line
-
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
     if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
         break
-    if answer_state in list_states:
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
         t = turtle.Turtle()
         t.hideturtle()
         t.penup()
         state_data = data[data.state == answer_state]
         t.goto(int(state_data.x), int(state_data.y))
-        t.write(state_data.state.item())
-        corrects += 1
-        list_states.remove(answer_state)
-        list_correct.append(answer_state)
-
-        if corrects == 5:
-            game_is_on = False
-            t.goto(0, 0)
-            t.write("YOU WIN!", font=('Arial', 40, 'normal'))
-
-df = pandas.DataFrame(list_states)
-df.to_csv("./missed_states.csv")
+        t.write(answer_state)
