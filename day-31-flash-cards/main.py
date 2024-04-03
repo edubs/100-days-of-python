4acd69f380756404da595f3ae04b6f20f1da89e2
edubs <11930020+edubs@users.noreply.github.com>
2024-04-03 11:19:33 -0400

    31 - flashy complete

--- day-31-flash-cards/main.py
+++ day-31-flash-cards/main.py
@@ -4,16 +4,45 @@ from tkinter import *
 import pandas
 
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+to_learn = {}
 
-# --------------------------- Read Data ------------------------------- #
-df = pandas.read_csv("data/english-german.csv")
-data_dict = df.to_dict(orient="records")
+# ------------------------- Read CSV into DataFrame ---------------------------- #
+
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/english-german.csv")
+    print(original_data)
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 
 # ------------------------- Data Functions ---------------------------- #
 def generate_word():
-    word_pair = random.choice(data_dict)
-    canvas.itemconfigure(german_word, text=word_pair["German"])
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="German", fill="black")
+    canvas.itemconfig(card_word, text=current_card["German"])
+    canvas.itemconfig(card_background, image=front_image)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"])
+    canvas.itemconfig(card_background, image=back_image)
+
+
+def known_word():
+    global current_card
+    to_learn.remove(current_card)
+    print(len(to_learn))
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    generate_word()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -21,20 +50,26 @@ def generate_word():
 window = Tk()
 window.title("German/English Flashcards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+flip_timer = window.after(3000, func=flip_card)
+
 canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
-canvas.grid(row=0, column=0, columnspan=2)
 front_image = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=front_image)
-english = canvas.create_text(400, 150, text="German", fill="black", font=("Ariel", 40, "italic"))
-german_word = canvas.create_text(400, 263, text="Deutsch", fill="black", font=("Ariel", 80, "bold"))
-print(canvas.itemcget(german_word, "text"))
+back_image = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=front_image)
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 80, "bold"))
+canvas.grid(row=0, column=0, columnspan=2)
 
 # Buttons
 wrong_image = PhotoImage(file="images/wrong.png")
 wrong_button = Button(image=wrong_image, borderwidth=0, highlightthickness=0, command=generate_word)
 wrong_button.grid(row=1, column=0)
+
 right_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_image, borderwidth=0, highlightthickness=0, command=generate_word)
+right_button = Button(image=right_image, borderwidth=0, highlightthickness=0, command=known_word)
 right_button.grid(row=1, column=1)
+
+generate_word()
 
 window.mainloop()
