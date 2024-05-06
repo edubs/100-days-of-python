bad3f0d36f7d0677b0cf8c7ebfe454f505e63209
edubs <11930020+edubs@users.noreply.github.com>
2024-05-06 14:44:52 -0400

    day 34

--- day-34-quiz-app/data.py
+++ day-34-quiz-app/data.py
@@ -1,1 +1,17 @@
-question_data = []
+import requests
+
+AMOUNT = 10
+TYPE = "boolean"
+DIFFICULTY = "hard"
+
+parameters = {
+    "amount": AMOUNT,
+    "type": TYPE,
+    "difficulty": DIFFICULTY,
+}
+
+response = requests.get("https://opentdb.com/api.php", params=parameters)
+response.raise_for_status()
+data = response.json()
+question_data = data["results"]
+print(question_data)
--- day-34-quiz-app/main.py
+++ day-34-quiz-app/main.py
@@ -1,34 +1,8 @@
 from data import question_data
 from question_model import Question
 from quiz_brain import QuizBrain
-import requests
-import json
+from ui import QuizInterface
 
-AMOUNT = 10
-TYPE = "boolean"
-CATEGORY = 18
-DIFFICULTY = "hard"
-
-
-def get_questions():
-    parameters = {
-        "amount": AMOUNT,
-        "type": TYPE,
-        "category": CATEGORY,
-        "difficulty": DIFFICULTY,
-    }
-    response = requests.get("https://opentdb.com/api.php", params=parameters, verify=False)
-    response.raise_for_status()
-    data = response.json()
-    question_data = data["results"]
-    # print(question_data)
-
-    with open('data.py', 'w', encoding='utf-8') as f:
-        f.write(f"question_data = ")
-        f.write(json.dumps(question_data, ensure_ascii=False, indent=4))
-
-
-get_questions()
 question_bank = []
 
 for q in question_data:
@@ -36,8 +10,10 @@ for q in question_data:
     question_bank.append(question)
 
 quiz = QuizBrain(question_bank)
-while quiz.still_has_questions():
-    quiz.next_question()
+quiz_ui = QuizInterface(quiz)
+
+# while quiz.still_has_questions():
+#     quiz.next_question()
 
 print("You've completed the quiz.")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
