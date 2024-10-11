a9c6545866b661d1854c4cdf15b9d724f5979924
Erik Williams <Erik.Williams@LibertyMutual.com>
2024-10-11 11:07:43 -0400

    DAY 46 - start

---
+++ day-46-billboard-100/main.py
@@ -0,0 +1,3 @@
+user_date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD")
+
+header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
